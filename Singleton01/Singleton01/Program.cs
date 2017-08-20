8d58b9b431efd78867d0358e00f59e2a9afd7a2f
jaimindave1 <31184238+jaimindave1@users.noreply.github.com>
2017-08-20 19:53:08 +0530

    Add files via upload

---
+++ Singleton01/Singleton01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton obj = Singleton.GetInstance();
+            obj.PrintMessage("Hello world");
+
+            Singleton obj2 = Singleton.GetInstance();
+            obj2.PrintMessage("Hello world");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton01/Singleton01/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton01
+{
+    public sealed class Singleton
+    {
+        private static int Counter = 0;
+        
+        private static Singleton _singletonObj= null;
+
+        public static Singleton GetInstance()
+        {
+            if (_singletonObj == null)
+                _singletonObj = new Singleton();
+            return _singletonObj;
+        }
+
+        private Singleton()
+        {   
+            Counter++;
+        }
+
+        public void PrintMessage(string message) 
+        {
+            Console.WriteLine(message + " Object " + Counter);
+        }
+    }
+}
